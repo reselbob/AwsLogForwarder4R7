bc564c14f6bd67fae7498a5b6e399acebd099cf3
reselbob <reselbob@gmail.com>
2018-01-14 10:43:52 -0800

    Added commenting

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const  zlib = require('zlib');
 var Logger = require('r7insight_node');
+/*****************************************************
+You'll need to set an environment variable, token, that describes your
+access token that is associated with the InsightOps log to
+which the AWS CloudWatch log will be forwarded. Also, you need
+to set the environment variable, region, that describes of the
+Rapid 7 Insight Ops log, usually us or en.
+*****************************************************/
 var logger = new Logger({ token: process.env.token , region: process.env.region})
 exports.handler = (event, context, callback) => {
-    // TODO implement
-    //let data = 'unknown';
-    if(event.awslogs.data){
-        //console.log(event.awslogs.data);
+    //make sure the data expected from AWS CloudWatch is present
+    if(event.awslogs && event.awslogs.data){
+        //The log forwarding is going to pass the data in gzip compression,
+        //thus, we need to unzip it.
         zlib.gunzip(Buffer.from(event.awslogs.data, 'base64'), function(err, uncompressedLogMessage) {
             if(err) {
                 console.error(err);
@@ -14,22 +21,18 @@ exports.handler = (event, context, callback) => {
             }
             else {
                 const str = uncompressedLogMessage.toString('utf-8');
+                //keep a local copy of the log entry in AWS
                 console.log(str);
+                //send the log entry onto InsightsOps
                 logger.info(str);
-                callback(null,uncompressedLogMessage.toString('utf-8') );
+                callback(null,str);
             }
         });
     } else
     {
-        const err = new Error('Not present, event.awslogs.data');
+        const err = new Error('Property,event.awslogs.data, is not present.');
         console.error(err);
         logger.err(JSON.stringify(err))
         callback(err);
     }
 };
-
-function logit(msg){
-
- 
-
-}
